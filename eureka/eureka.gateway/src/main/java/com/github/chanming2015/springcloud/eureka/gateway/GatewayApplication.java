1d4644839eb8bff1704d848f2bb977c15f6b9e9e
chanming2015 <476076812@qq.com>
2021-01-14 19:38:19 +0800

    eureka.gateway init.

---
+++ eureka/eureka.gateway/src/main/java/com/github/chanming2015/springcloud/eureka/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.github.chanming2015.springcloud.eureka.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: 服务网关 <br/> 
+ * Create Date:2021年1月14日 <br/> 
+ * @author XuMaoSen
+ */
+@SpringBootApplication
+public class GatewayApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
